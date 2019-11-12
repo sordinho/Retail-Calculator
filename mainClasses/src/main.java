cab459dc358e4286cfbdfc96e72c9e7bbd8c5778
francesco <frenky.riba.95@gmail.com>
2019-11-12 15:38:23 +0100

    input

--- mainClasses/src/main.java
+++ mainClasses/src/main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 class main{
 
+
     public static void main(String[]args){
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter the number of products");
+        int myint = keyboard.nextInt();
+
+
         System.out.println("Hello World!");
     }
 }
